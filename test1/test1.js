4a86402f720484330f16024eb32b992826165f65
binhzonzon25 <88417591+binhzonzon25@users.noreply.github.com>
2021-09-11 15:25:22 +0700

    Add files via upload

---
+++ test1/test1.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const{hostname}=require('os');
+const host ='localhost';
+const port = process.env.PORT || 5000;
+const server = http.createServer((req, res)=>
+{
+    res.statusCode =200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Cloud computing class!');
+});
+server.listen(port, hostname, ()=>
+{
+    console.log(`Sever is running at ${port}`);
+})  
